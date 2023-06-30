9e661f36811e13763766bb18d69beb672731bec2
NikhilAtyali <nikhilatyali90@gmail.com>
2023-07-01 00:57:38 +0530

    Feat: added login and signup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import  Login  from "./Login"
+import { Signup } from "./Signup";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Signup.jsx
@@ -0,0 +1,60 @@
+import { Link } from "react-router-dom";
+import Lottie from "lottie-react";
+import signup from "./animation/signup.json";
+import "./Signup.css";
+
+export const Signup = () =>{
+    return (
+        <>
+        <div className="signup-container">
+        <Lottie animationData={signup} loop={true} />
+        <form className="signup-form">
+          <h1 className="signup-form-heading">Sign up</h1>
+          <input
+            className="signup-form-input"
+            type="text"
+            required
+            placeholder="First Name"
+          />
+          <input
+            className="signup-form-input"
+            type="text"
+            required
+            placeholder="Last Name"
+          />
+          <input
+            className="signup-form-input"
+            type="text"
+            required
+            placeholder="Username"
+          />
+          <input
+            className="signup-form-input"
+            type="password"
+            required
+            placeholder="Password"
+          />
+          <input
+            className="signup-form-input"
+            type="password"
+            required
+            placeholder="Confirm Password"
+          />
+          <label
+            className="signup-show-password"
+            htmlFor="signup_show_password"
+          >
+            <input id="signup_show_password" type="checkbox" />
+            Show Password
+          </label>
+          <button className="signup-btn" type="submit">
+            Create Account
+          </button>
+          <Link className="login-signup-link" to="/login">
+            Already have an account?
+          </Link>
+        </form>
+      </div>
+        </>
+    )
+}
