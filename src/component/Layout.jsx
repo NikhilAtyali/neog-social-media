651b28ad9f3f698113681569c62428906c5d56e3
NikhilAtyali <nikhilatyali90@gmail.com>
2023-07-01 04:39:21 +0530

    Feat: Follow suggestion added

--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router";
 import { Navbar } from "./Navbar";
 import { Sidebar } from "./Sidebar";
+import { SuggestionBar } from "./SuggestionBar";
 export const Layout = () => {
   return (
     <>
       <Navbar />
       <Sidebar />
-      <Outlet />
+      <SuggestionBar />
+      {/* <Outlet /> */}
     </>
   );
 };
